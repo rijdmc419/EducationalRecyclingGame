2e505c16d9815fdf619ecfaae4268969a09418ad
Lila <lila.schweinfurth@gmail.com>
2020-07-22 14:38:49 -0700

    created menu

---
+++ EducationalRecyclingGame/Assets/LS_MenuFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LS_MenuFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play() {
+    	SceneManager.LoadScene(sceneName: "LS_Test_Scene");
+    }
+
+    public void Options() {
+    	SceneManager.LoadScene(sceneName: "LS_Options_Scene");
+    }
+
+    public void Quit() {
+    	Application.Quit();
+    }
+}
---
+++ EducationalRecyclingGame/Assets/LS_SetFont.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LS_SetFont : MonoBehaviour {
+    
+    public Font otherFont;
+
+    void Start() {
+    	var textObjects = FindObjectsOfType<Text>();
+    	foreach (Text text in textObjects) {
+    		text.font = otherFont;
+    	}
+    	
+    }
+}
