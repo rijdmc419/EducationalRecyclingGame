8b6905c35c2de260ffa89dfaaa112a0749fa14ed
Elaine <esdemetrion@gmail.com>
2020-07-23 10:43:52 -0700

    Added Alan's DragAndDrop script and attached it to a test block. Looks like everything works!

---
+++ EducationalRecyclingGame/Assets/DragAndDrop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+class DragAndDrop : MonoBehaviour
+{
+    private Color mouseOverColor = Color.red;
+    private Color originalColor = Color.green;
+    private bool dragging = false;
+    private float distance;
+    private Vector3 startDist;
+
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 rayPoint = ray.GetPoint(distance);
+        startDist = transform.position - rayPoint;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint + startDist;
+        }
+    }
+}
