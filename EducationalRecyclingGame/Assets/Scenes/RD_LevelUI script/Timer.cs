4ce6f92a4b6c5ee1154a1ddc0197751a74b3d955
Rij Dorfman <rdorfman419@outlook.com>
2020-07-23 10:31:05 -0700

    Timer and Score work

---
+++ EducationalRecyclingGame/Assets/Scenes/RD_LevelUI script/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    int timeleft = 60;
+    string time_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timer = GetComponent<Text>();
+        InvokeRepeating("Countdown", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Countdown()
+    {
+        time_text = timeleft.ToString();
+        timer.text = time_text;
+        print(timeleft);
+        timeleft--;
+    }
+}
