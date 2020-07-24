4c8e4b60f35b344f35ac694e374f779816c18940
Elaine <esdemetrion@gmail.com>
2020-07-24 11:53:28 -0700

    Spawner is working in my scene. I still want to do things to it, but I wanted to commit before I mess this up.

---
+++ EducationalRecyclingGame/Assets/ConveyorBeltAssets/SpawnItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SpawnItems : MonoBehaviour
+{
+    public Transform itemPrefab;
+    const double timeBtwnSpawns = 1;
+    double timeLeftBeforeNextSpawn;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeLeftBeforeNextSpawn -= Time.deltaTime;
+
+        //every so often...
+        if(timeLeftBeforeNextSpawn <= 0)
+        {
+            Transform item = Instantiate(itemPrefab);
+            item.localPosition = new Vector3(3, 2, 0);
+            //the item could set itself up in its "Awake" function
+            //OR we could set it up here. Depends.
+
+            timeLeftBeforeNextSpawn = timeBtwnSpawns;
+        }
+
+    }
+}
