01fe7640ac3c5a351cddb99a5fe5d7bac7533fe2
Lila <lila.schweinfurth@gmail.com>
2020-07-27 11:29:45 -0700

    added script to change speed

---
+++ EducationalRecyclingGame/Assets/ConveyorBeltAssets/LS_ConveyorBelt.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LS_ConveyorBelt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int level = 1;
+    public float speed = -2f;
+    float conveyorVelocity;
+    public GameObject obj;
+    SurfaceEffector2D se;
+
+    void Start() {
+    	obj.AddComponent<SurfaceEffector2D>();
+    	se = obj.GetComponent<SurfaceEffector2D>();
+    	if (se == null) {
+    		print(true);
+    	}
+        InvokeRepeating("LevelChange", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    public void LevelChange() {
+    	if (level!=SendInfo.levelNumber) {
+    		ChangeSpeed();
+    	}
+    }
+
+    void ChangeSpeed() {
+    	level = SendInfo.levelNumber;
+    	if (level > 5) {
+    		speed = (level-2) * -1f;
+    		print(speed);
+    	}
+    	se.speed = speed;
+    	se.forceScale = 0.1f;
+    	se.useColliderMask = true;
+
+
+    }
+}
