0e7782450e430adefc93c9e37f8d48e7ee880193
Elaine <esdemetrion@gmail.com>
2020-07-27 10:41:38 -0700

    Two new scripts + random tag
    *Static class to hold tag names
    *ItemManager script. Currently has one function to randomly set up an item with a random tag.
    *In the future, we might want to change where tags are stored

--- EducationalRecyclingGame/Assets/ConveyorBeltAssets/SpawnItems.cs
+++ EducationalRecyclingGame/Assets/ConveyorBeltAssets/SpawnItems.cs
@@ -6,9 +6,8 @@ using System;
 public class SpawnItems : MonoBehaviour
 {
     public Transform itemPrefab;
-    const double timeBtwnSpawns = 1;
+    const double timeBtwnSpawns = 2;
     double timeLeftBeforeNextSpawn;
-
 
     // Update is called once per frame
     void FixedUpdate()
@@ -19,9 +18,8 @@ public class SpawnItems : MonoBehaviour
         if(timeLeftBeforeNextSpawn <= 0)
         {
             Transform item = Instantiate(itemPrefab);
-            item.localPosition = new Vector3(3, 2, 0);
-            //the item could set itself up in its "Awake" function
-            //OR we could set it up here. Depends.
+            //access the item's script
+            item.GetComponent<ItemManager>().setUpRandomItem(); 
 
             timeLeftBeforeNextSpawn = timeBtwnSpawns;
         }
