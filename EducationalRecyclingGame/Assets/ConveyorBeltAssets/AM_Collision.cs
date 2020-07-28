027594e31f23435d7ee2911075baf98829c7a9d2
Lila <lila.schweinfurth@gmail.com>
2020-07-28 14:37:56 -0700

    collision w/ random sprite and memory

---
+++ EducationalRecyclingGame/Assets/ConveyorBeltAssets/AM_Collision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AM_Collision : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Debug.Log("hit detected " + coll.gameObject.name);
+        if (coll.gameObject.name == "Bin")
+        {
+            Destroy(this.gameObject);
+            // this.gameObject.SetActive(false);
+        }
+        
+    }
+}
