fdd07f8f9103870e0dbf0decd2ff51bc6db83fe5
Elaine <esdemetrion@gmail.com>
2020-07-30 15:11:35 -0700

    Saving progress
    Literally just saving progress. The mess of files in this commit should not be pulled by anyone ever...

---
+++ EducationalRecyclingGame/Assets/RD_LevelUI script/TutorialFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
