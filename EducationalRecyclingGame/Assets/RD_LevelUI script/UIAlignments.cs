1a07a8e08b8d73326cefcb370e05782ecd674345
Rij Dorfman <rdorfman419@outlook.com>
2020-07-31 10:55:54 -0700

    Fixed UI again
    need bins to be adjusted

--- EducationalRecyclingGame/Assets/RD_LevelUI script/UIAlignments.cs
+++ EducationalRecyclingGame/Assets/RD_LevelUI script/UIAlignments.cs
@@ -21,19 +21,19 @@ public class UIAlignments : MonoBehaviour
         background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1400);
         background.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 130);
 
-        RectTransform Pause_background = PauseCanvas.transform.Find("Pause Background").GetComponent<RectTransform>();
-        background.anchorMin = new Vector2(0.5f, 0.5f);
-        background.anchorMax = new Vector2(0.5f, 0.5f);
-        background.pivot = new Vector2(0.5f, 0.5f);
-        background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1740);
-        background.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 930);
+        RectTransform Pause_background = PauseCanvas.transform.Find("PauseBackground").GetComponent<RectTransform>();
+        Pause_background.anchorMin = new Vector2(0.5f, 0.5f);
+        Pause_background.anchorMax = new Vector2(0.5f, 0.5f);
+        Pause_background.pivot = new Vector2(0.5f, 0.5f);
+        Pause_background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1740);
+        Pause_background.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 930);
 
-        RectTransform Complete_background = NextCanvas.transform.Find("Complete Background").GetComponent<RectTransform>();
-        background.anchorMin = new Vector2(0.5f, 0.5f);
-        background.anchorMax = new Vector2(0.5f, 0.5f);
-        background.pivot = new Vector2(0.5f, 0.5f);
-        background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1340);
-        background.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 740);
+        RectTransform Complete_background = NextCanvas.transform.Find("CompleteBackground").GetComponent<RectTransform>();
+        Complete_background.anchorMin = new Vector2(0.5f, 0.5f);
+        Complete_background.anchorMax = new Vector2(0.5f, 0.5f);
+        Complete_background.pivot = new Vector2(0.5f, 0.5f);
+        Complete_background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1340);
+        Complete_background.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 740);
 
 
     }
